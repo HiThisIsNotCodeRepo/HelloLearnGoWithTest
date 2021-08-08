a0b0d17dd6b6c66bf6b9b007ecaf91941b7e303d
qinchenfeng <freddy.qin@gmail.com>
2021-08-08 17:04:50 +0800

    Refactor and add more subtest to "Hello, World".

--- 1_Go_fundamentals/2_Hello_world/hello/hello.go
+++ 1_Go_fundamentals/2_Hello_world/hello/hello.go
@@ -1,11 +1,28 @@
 package hello
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello is the data exists in domain
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
