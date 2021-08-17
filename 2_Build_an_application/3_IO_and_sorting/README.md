15c8c2c7e487eb7b14ed3e8cc18ce1a0becf4db5
qinchenfeng <freddy.qin@gmail.com>
2021-08-17 21:50:58 +0800

    Add files for "io and sorting".

---
+++ 2_Build_an_application/3_IO_and_sorting/README.md
@@ -0,0 +1,67 @@
+# IO and sorting
+
+## Thread safe implementation
+
+```
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"sync"
+)
+
+type FileSystemPlayerStore struct {
+	mu       sync.Mutex
+	lock     bool
+	database io.ReadWriteSeeker
+}
+
+func (f *FileSystemPlayerStore) GetLeague() League {
+	if !f.lock {
+		f.mu.Lock()
+		defer func() {
+			f.mu.Unlock()
+			f.lock = false
+		}()
+	}
+
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	f.mu.Lock()
+	f.lock = true
+	defer func() {
+		f.lock = false
+		f.mu.Unlock()
+	}()
+
+	player := f.GetLeague().Find(name)
+	if player != nil {
+		return player.Wins
+	}
+	return 0
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	f.mu.Lock()
+	f.lock = true
+	defer func() {
+		f.lock = false
+		f.mu.Unlock()
+	}()
+	league := f.GetLeague()
+	player := league.Find(name)
+	if player != nil {
+		player.Wins++
+	} else {
+		league = append(league, Player{name, 1})
+	}
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
+}
+
+```
---
+++ 2_Build_an_application/3_IO_and_sorting/server_integration_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	database, cleanDatabase := createTempFile(t, "")
+	defer cleanDatabase()
+	store := &FileSystemPlayerStore{database: database}
+	server := NewPlayerServer(store)
+	player := "Pepper"
+	var wg sync.WaitGroup
+	wg.Add(1000)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+	assertResponseBody(t, response.Body.String(), "1000")
+}
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
+}
+func newPostWinRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
